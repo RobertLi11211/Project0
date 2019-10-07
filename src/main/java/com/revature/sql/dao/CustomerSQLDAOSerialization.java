8f959bf536255490292ef2454a7f098c8d91b925
Robert Li <bobli2011@gmail.com>
2019-10-07 12:04:05 -0400

    testing

--- src/main/java/com/revature/sql/dao/CustomerSQLDAOSerialization.java
+++ src/main/java/com/revature/sql/dao/CustomerSQLDAOSerialization.java
@@ -1,18 +1,27 @@
 package com.revature.sql.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.pojos.Customer;
 import com.revature.util.ConnectionFactory;
 
 public class CustomerSQLDAOSerialization implements CustomerSQLDAO {
-	String sql = "select * from CarCustomers where username = ?";
 	private static Connection conn = ConnectionFactory.getConnection();
 
 	@Override
 	public Customer getCustomer(String username) {
-
+		String sql = "select * from CarCustomers where username = ?";
+		Customer cust = new Customer();
+		PreparedStatement stat = conn.prepareStatement(sql);
+		try {
+			stat.setString(1, username);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
